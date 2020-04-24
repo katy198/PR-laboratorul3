eae516359aebd4eacc2715ffb43651b6e7c71573
Ecaterina Pavlenco <katy.pey@mail.ru>
2020-04-24 14:15:35 +0300

    first comit

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(22, 39);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 0;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(150, 39);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(47, 132);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Get";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(316, 129);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Post";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(350, 36);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Head";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(350, 80);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Option";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(79, 65);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(111, 23);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Authentification";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(431, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(232, 126);
+            this.richTextBox1.TabIndex = 7;
+            this.richTextBox1.Text = "";
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(22, 158);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(641, 376);
+            this.webBrowser1.TabIndex = 8;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(210, 132);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(683, 554);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Name = "Form1";
+            this.Text = "HTTP Client";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            webBrowser1.ScriptErrorsSuppressed = true;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (textBox2.Text != "" && textBox3.Text != "")
+            {
+                Authorization(textBox2.Text, textBox3.Text);
+            }
+            else
+            {
+                MessageBox.Show("Insert login/password", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+        static void Authorization(string login, string password)
+        {
+            if (File.Exists("response.txt"))
+            {
+                File.Delete("response.txt");
+            }
+
+            var cookieContainer = new CookieContainer();
+
+            using (var handler = new HttpClientHandler { CookieContainer = cookieContainer })
+            using (var client = new HttpClient(handler))
+            {
+                var values = new Dictionary<String, String>
+                {
+                    { "nick", login },
+                    { "pass", password }
+                };
+
+                var content = new FormUrlEncodedContent(values);
+                var response = client.PostAsync("https://www.bestseller.md/", content)
+                   .GetAwaiter()
+                   .GetResult();
+                var responseString = response.Content.ReadAsStringAsync();
+                Uri uri = new Uri("https://www.bestseller.md/");
+                var c = cookieContainer.GetCookies(uri);
+
+                for (int i = 0; i < c.Count; i++)
+                {
+                    File.AppendAllText("response.txt", c[i].Name, Encoding.UTF8);
+                    File.AppendAllText("response.txt", "=", Encoding.UTF8);
+                    File.AppendAllText("response.txt", c[i].Value + "\n", Encoding.UTF8);
+
+
+                }
+                if (File.Exists("response.txt"))
+                    Process.Start("response.txt");
+            }
+        }
+
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            var cookieContainer = new CookieContainer();
+            if (File.Exists("response.txt"))
+            {
+                string[] cookies = File.ReadAllLines(@"response.txt", System.Text.Encoding.Default);
+
+                foreach (string cookie1 in cookies)
+                    cookieContainer.SetCookies(new Uri("https://www.bestseller.md/"), cookie1);
+
+            }
+            Uri uri = new Uri("https://www.bestseller.md/");
+            var values1 = new Dictionary<String, String>
+            {
+
+            };
+            var content1 = new FormUrlEncodedContent(values1);
+            using (var handler1 = new HttpClientHandler { CookieContainer = cookieContainer })
+            using (var client1 = new HttpClient(handler1))
+            {
+
+                using (HttpResponseMessage response = await client1.GetAsync(uri))
+                {
+
+                    var s = response.Headers.ToString();
+                    richTextBox1.Text = s;
+                    using (HttpContent content = response.Content)
+                    {
+                        string mycontent = await content.ReadAsStringAsync();
+
+                        webBrowser1.DocumentText = mycontent;
+                    }
+
+                }
+
+            }
+
+        }
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            var cookieContainer = new CookieContainer();
+            if (File.Exists("response.txt"))
+            {
+                string[] cookies = File.ReadAllLines(@"response.txt", System.Text.Encoding.Default);
+
+                foreach (string cookie1 in cookies)
+                    cookieContainer.SetCookies(new Uri("https://www.bestseller.md/"), cookie1);
+
+            }
+            Uri uri = new Uri("https://www.bestseller.md/");
+            var values1 = new Dictionary<String, String>
+            {
+                { "usearch", textBox4.Text.ToString()},
+                { "serch", "2" }
+
+            };
+            var content1 = new FormUrlEncodedContent(values1);
+            using (var handler1 = new HttpClientHandler { CookieContainer = cookieContainer })
+            using (var client1 = new HttpClient(handler1))
+            {
+
+                using (HttpResponseMessage response = await client1.PostAsync(uri, content1))
+                {
+
+                    var s = response.Headers.ToString();
+                    richTextBox1.Text = s;
+                    using (HttpContent content = response.Content)
+                    {
+                        string mycontent = await content.ReadAsStringAsync();
+
+                        webBrowser1.DocumentText = mycontent;
+                    }
+
+                }
+
+            }
+
+        }
+
+
+
+    private async void button4_Click(object sender, EventArgs e)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                Uri url = new Uri("http://unite.md");
+                using (HttpResponseMessage response = await client.SendAsync(new HttpRequestMessage(HttpMethod.Options, url)))
+                {
+                    richTextBox1.Text = response.ToString();
+                }
+
+
+            }
+
+        }
+
+        private async void  button3_Click(object sender, EventArgs e)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                Uri url = new Uri("https://www.bestseller.md/");
+                using (HttpResponseMessage response = await client.SendAsync(new HttpRequestMessage(HttpMethod.Head, url)))
+                {
+                    richTextBox1.Text = response.ToString();
+                }
+
+
+            }
+
+
+        }
+    }
+}
